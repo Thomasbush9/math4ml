d848acd383abce44895a6abe913aaf7b9a6afd9c
thomas bush <thomasbush52@gmail.com>
2025-09-13 16:29:45 +0200

    added from data

--- linear_algebra/utils.py
+++ linear_algebra/utils.py
@@ -1,11 +1,9 @@
 import random
 
-
 class Matrix():
     def __init__(self, shape:tuple) -> None:
         self.shape = shape
-        rows, cols = shape
-        self.data = [[random.uniform(0, 1) for _ in range(cols)] for _ in range(rows)]
+        self.data = None
     def __repr__(self) -> str:
         return "\n".join(str(row) for row in self.data)
 
@@ -15,6 +13,17 @@ class Matrix():
     def __setitem__(self, idx, value):
         self.data[idx] = value
 
+    @classmethod
+    def from_data(cls, data, copy=True, strict=True):
+        if strict:
+            assert all(type(val) == type(data[0]) for col in data for val in col), "Elements must be same type!"
+        shape = (len(data), len(data[0]))
+        temp = cls(shape)
+        if copy:
+            temp.data = data.copy()
+        return temp
+
+#TODO make it with a new matrix
     @property
     def T(self):
         rows, cols = self.shape
@@ -24,10 +33,32 @@ class Matrix():
                 m[i].append(e)
         self.data = m
     def __add__(self, x):
-        assert len(self.shape) == len(x.shape), "Matrices must have same shape"
         # add dim checker
+        if isinstance(x, int|float):
+            temp_m = Matrix((self.shape))
+            temp_m.data = [[e + x for e in col] for col in self.data]
+            return temp_m
         if isinstance(x, Matrix):
+            assert self.shape == x.shape, "Matrices must have same shape"
             data = [[e1+e2 for e1, e2 in zip(col1, col2)] for col1, col2 in zip(self.data, x.data)]
+            shape= self.shape
+            temp_m = Matrix(shape)
+            temp_m.data= data
+            return temp_m
+    def __radd__(self, x):
+    #TODO check the type to be the same in the future
+        temp_m = Matrix(self.shape))
+        temp_m.data = [[e + x for e in col] for col in self.data]
+        return temp_m
+    def __sub__(self, x):
+        # add dim checker
+        if isinstance(x, int|float):
+            temp_m = Matrix((self.shape))
+            temp_m.data = [[e - x for e in col] for col in self.data]
+            return temp_m
+        if isinstance(x, Matrix):
+            assert self.shape == x.shape, "Matrices must have same shape"
+            data = [[e1-e2 for e1, e2 in zip(col1, col2)] for col1, col2 in zip(self.data, x.data)]
             shape= self.shape
             temp_m = Matrix(shape)
             temp_m.data= data
