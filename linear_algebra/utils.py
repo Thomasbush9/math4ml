6fc1f77087b4b3bdf6342167a23fb6a963dcb9bf
thomas bush <thomasbush52@gmail.com>
2025-09-13 11:43:53 +0200

    added addition support

--- linear_algebra/utils.py
+++ linear_algebra/utils.py
@@ -23,6 +23,15 @@ class Matrix():
             for i, e in enumerate(row):
                 m[i].append(e)
         self.data = m
+    def __add__(self, x):
+        assert len(self.shape) == len(x.shape), "Matrices must have same shape"
+        # add dim checker
+        if isinstance(x, Matrix):
+            data = [[e1+e2 for e1, e2 in zip(col1, col2)] for col1, col2 in zip(self.data, x.data)]
+            shape= self.shape
+            temp_m = Matrix(shape)
+            temp_m.data= data
+            return temp_m
 
 
 
