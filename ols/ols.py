25ba0c4bfe26bba31a1ac872be6a89841b29b8d6
thomas bush <thomasbush52@gmail.com>
2025-10-25 23:54:50 +0200

    init min of a given norm using QR

--- ols/ols.py
+++ ols/ols.py
@@ -6,6 +6,13 @@ def pseudo_inverse(A: np.ndarray) -> np.ndarray:
     U, SIGMA, V_t = np.linalg.svd(A)
     return (U.T, 1 / SIGMA, V_t.T)
 
+def minimize_norm(A:np.ndarray, norm:str)->np.ndarray:
+    """Function to minimize the given norm of a matrix A"""
+    U_t, SIGMA_inv, V = pseudo_inverse(A)
+    if norm == 'l2':
+        return U_t @ np.diag(SIGMA_inv) @ V
+    else:
+        raise NotImplementedError(f"Norm '{norm}' not implemented.")
 
 if __name__ == "__main__":
     A = np.random.randn(12, 8)
